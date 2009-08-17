2eb77999dc32d7ed7004ddb6b443508e93fb8a68
Justin Balthrop <justin@geni.com>
2009-08-17 15:17:49 -0700

    add some tests

--- test/icunicode_test.rb
+++ test/icunicode_test.rb
@@ -1,7 +1,16 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class UnicodeCollationTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "sort using unicode collation" do
+    assert_equal ["cafe", "café", "cafes"], ["cafe", "cafes", "café"].sort_by {|s| s.unicode_sort_key}
+    assert_equal ["role", "Role", "rôle"],  ["rôle", "role", "Role"].sort_by {|s| s.unicode_sort_key}
+    assert_equal ["cote", "coté", "côte", "côté"], ["côté", "coté", "cote", "côte"].sort_by {|s| s.unicode_sort_key}
+  end
+
+  should "transliterate" do
+    assert_equal "ムクドナルデ'ス",         "mcdonald's".transliterate('Katakana')
+    assert_equal "さむらい",               "samurai".transliterate('Hiragana')
+    assert_equal "θε γρεατ γρεεκ",         "the great greek".transliterate('Greek')
+    assert_equal "фром руссиа уитх лове", "from russia with love".transliterate('Cyrillic')
   end
 end
